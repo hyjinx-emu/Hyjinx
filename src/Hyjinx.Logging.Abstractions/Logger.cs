d17f31546d4ad6a1df8516c7676ed522c855dc31
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-03-19 18:48:51 -0400

    Cleanup.

--- src/Hyjinx.Logging.Abstractions/Logger.cs
+++ src/Hyjinx.Logging.Abstractions/Logger.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.Logging;
 using System;
-using System.Diagnostics;
 
 namespace Hyjinx.Common.Logging;
 
@@ -8,7 +7,7 @@ namespace Hyjinx.Common.Logging;
 /// A mechanism for logging operations within the emulator.
 /// </summary>
 /// <remarks>This class uses <see cref="LogClass"/> to determine the event type for a particular log event.</remarks>
-public class Logger : ILog
+public static class Logger
 {
     /// <summary>
     /// Gets the default logger.
@@ -30,104 +29,5 @@ public class Logger : ILog
         
         DefaultLoggerFactory = factory;
         DefaultLogger = factory.CreateLogger("Program");
-    }
-
-    // public static ILog? Debug { get; private set; }
-    // public static ILog? Info { get; private set; }
-    public static ILog? Warning { get; private set; }
-    // public static ILog? Error { get; private set; }
-    // public static ILog? Guest { get; private set; }
-    // public static ILog? AccessLog { get; private set; }
-    // public static ILog? Stub { get; private set; }
-    // public static ILog? Trace { get; private set; }
-    // public static ILog Notice { get; private set; } = null!;
-
-    private readonly ILogger logger;
-    private readonly LogLevel logLevel;
-
-    private Logger(ILogger logger, LogLevel logLevel)
-    {
-        this.logger = logger;
-        this.logLevel = logLevel;
-    }
-
-    public static void SetEnable(LogLevel logLevel, bool enabled)
-    {
-        switch (logLevel)
-        {
-            // case LogLevel.Debug: 
-            //     Debug = enabled ? new Logger(DefaultLogger, LogLevel.Debug) : null; 
-            //     break;
-            
-            // case LogLevel.Info:
-            //     Info = enabled ? new Logger(DefaultLogger, Microsoft.Extensions.Logging.LogLevel.Information) : null;
-            //     break;
-            
-            case LogLevel.Warning:
-                Warning = enabled ? new Logger(DefaultLogger, LogLevel.Warning) : null;
-                break;
-            
-            // case LogLevel.Error:
-            //     Error = enabled ? new Logger(DefaultLogger, Microsoft.Extensions.Logging.LogLevel.Error) : null;
-            //     break;
-            
-            // case LogLevel.Guest:
-            //     Guest = enabled ? new Logger(DefaultLogger, Microsoft.Extensions.Logging.LogLevel.Debug) : null;
-            //     break;
-            
-            // case LogLevel.AccessLog:
-            //     AccessLog = enabled ? new Logger(DefaultLogger, Microsoft.Extensions.Logging.LogLevel.Debug) : null;
-            //     break;
-            
-            // case LogLevel.Stub:
-            //     Stub = enabled ? new Logger(DefaultLogger, Microsoft.Extensions.Logging.LogLevel.Debug) : null;
-            //     break;
-            
-            // case LogLevel.Trace:
-            //     Trace = enabled ? new Logger(DefaultLogger, LogLevel.Trace) : null;
-            //     break;
-        }
-    }
-
-
-    public void PrintMsg(LogClass logClass, string message)
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    public void Print(LogClass logClass, string message, string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    public void Print(LogClass logClass, string message, object data, string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    [StackTraceHidden]
-    public void PrintStack(LogClass logClass, string message, string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    public void PrintStub(LogClass logClass, string message = "", string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    public void PrintStub(LogClass logClass, object data, string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), data.ToString());
-    }
-
-    public void PrintStub(LogClass logClass, string message, object data, string caller = "")
-    {
-        logger.Log(logLevel, new EventId((int)logClass, logClass.ToString()), message);
-    }
-
-    public void PrintRawMsg(string message)
-    {
-        logger.Log(logLevel, message);
     }
 }
