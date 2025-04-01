ec65a41d6ee3da4db2008d5c69be7b4fe1bc7f83
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-03-31 20:38:22 -0400

    Adding launch options.

---
+++ src/Hyjinx.UI.Common/AutoConfiguration/LaunchOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Hyjinx.UI.Common.AutoConfiguration;
+
+public record LaunchOptions
+{
+    [ConfigurationKeyName("-docked-mode")]
+    public bool? OverrideDockedMode { get; init; }
+    
+    [ConfigurationKeyName("hardware-acceleration")]
+    public bool? OverrideHardwareAcceleration { get; init; }
+    
+    [ConfigurationKeyName("graphics-backend")]
+    public string? OverrideGraphicsBackend { get; init; }
+    
+    [ConfigurationKeyName("hide-cursor")]
+    public string? OverrideHideCursor { get; init; }
+    
+    [ConfigurationKeyName("root-data-dir")]
+    public string? BaseDirPathArg { get; init; }
+    
+    [ConfigurationKeyName("profile")]
+    public string? Profile { get; init; }
+    
+    [ConfigurationKeyName("path")]
+    public string? LaunchPathArg { get; init; }
+    
+    [ConfigurationKeyName("application-id")]
+    public string? LaunchApplicationId { get; init; }
+    
+    [ConfigurationKeyName("fullscreen")]
+    public bool StartFullscreenArg { get; init; }
+    
+    [ConfigurationKeyName("config")]
+    public string? OverrideConfigFile { get; init; }
+}
