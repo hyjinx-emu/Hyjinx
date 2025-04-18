bcced99ca04f19bb548ad0d4c584a4e7a311fff1
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-04-18 08:17:02 -0400

    Exposing required method.

--- src/Hyjinx.LibHac/Crypto/Sha256.cs
+++ src/Hyjinx.LibHac/Crypto/Sha256.cs
@@ -3,7 +3,7 @@ using LibHac.Crypto.Impl;
 
 namespace LibHac.Crypto;
 
-internal static class Sha256
+public static class Sha256
 {
     public const int DigestSize = 0x20;
 
@@ -11,7 +11,7 @@ internal static class Sha256
     /// Creates an uninitialized SHA-256 <see cref="IHash"/> object.
     /// </summary>
     /// <returns> The new uninitialized SHA-256 <see cref="IHash"/> object.</returns>
-    public static IHash CreateSha256Generator()
+    internal static IHash CreateSha256Generator()
     {
         return new Sha256Generator();
     }
