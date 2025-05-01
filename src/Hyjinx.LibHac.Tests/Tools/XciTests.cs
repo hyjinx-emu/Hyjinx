9a1602320220ac6899a90b7697fb101fe781fa09
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-05-01 09:08:16 -0400

    Stubbing code.

--- src/Hyjinx.LibHac.Tests/Tools/XciTests.cs
+++ src/Hyjinx.LibHac.Tests/Tools/XciTests.cs
@@ -1,13 +1,25 @@
-﻿using System.IO;
+﻿using LibHac.Tools.Fs;
+using LibHac.Tools.FsSystem;
+using System.IO;
 
 namespace LibHac.Tests.Tools;
 
 public class XciTests : GameDecrypterTests
 {
-    protected override string TargetFileName => "";
+    protected override string TargetFileName => "New Super Mario Bros. U Deluxe [0100EA80032EA000][v0] [NKA][C][T].xci";
 
     protected override void DoDecrypt(FileInfo inFile, FileInfo destination)
     {
-        throw new System.NotImplementedException();
+        var keySet = CreateEncryptedKeySet();
+        
+        using var inStream = inFile.OpenRead();
+
+        var xci = new Xci(keySet, inStream.AsStorage());
+        using var outStream = destination.OpenWrite();
+        
+        var decrypter = new XciDecrypter(keySet);
+        decrypter.Decrypt(xci, outStream);
+
+        outStream.Flush();
     }
 }
---
+++ src/Hyjinx.LibHac/Tools/Fs/XciDecrypter.cs
@@ -0,0 +1,19 @@
+﻿#if IS_TPM_BYPASS_ENABLED
+#pragma warning disable 0618 // Type or member is obsolete
+
+using LibHac.Common.Keys;
+using System;
+using System.IO;
+
+namespace LibHac.Tools.Fs;
+
+public class XciDecrypter(KeySet keySet)
+{
+    public void Decrypt(Xci xci, Stream outStream)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+#pragma warning restore 0618 // Type or member is obsolete
+#endif
