5a6597cbbb55bdb1e6d04b5d1bb204b022c9d323
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-06-02 08:33:50 -0400

    Fixing constructor.

--- src/Hyjinx.LibHac/Common/Keys/KeySet.cs
+++ src/Hyjinx.LibHac/Common/Keys/KeySet.cs
@@ -3,13 +3,26 @@ using LibHac.FsSrv;
 // ReSharper disable once CheckNamespace
 namespace LibHac.Common.Keys;
 
+/// <summary>
+/// Describes a set of keys used by the host.
+/// </summary>
+/// <remarks>This class has been intentionally left blank.</remarks>
 public class KeySet
 {
+    /// <summary>
+    /// Defines an empty key set.
+    /// </summary>
     public static readonly KeySet Empty = new();
 
+    /// <summary>
+    /// Gets the external key set.
+    /// </summary>
     public ExternalKeySet ExternalKeySet { get; } = new();
 
-    internal KeySet() { }
+    /// <summary>
+    /// Initializes a new instance of the class.
+    /// </summary>
+    protected KeySet() { }
 
     /// <summary>
     /// Returns a new <see cref="KeySet"/> containing any keys that have been compiled into the library.
