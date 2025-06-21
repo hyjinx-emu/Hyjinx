21c5b44c54e6333b697bc9afa9b7c24bbfb59358
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-06-21 11:52:34 -0400

    Adding unit tests.

---
+++ src/Hyjinx.LibHac.Tests/Tools/FsSystem/StreamStorage2Tests.cs
@@ -0,0 +1,79 @@
+﻿using LibHac.Tools.FsSystem;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibHac.Tests.Tools.FsSystem;
+
+public class StreamStorage2Tests
+{
+    [Fact]
+    public async Task ReadsTheDataFromBeginning()
+    {
+        using var ms = new MemoryStream([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        var storage = new StreamStorage2(ms);
+        
+        var buffer = new Memory<byte>(new byte[10]);
+        var result = await storage.ReadAsync(buffer);
+        
+        Assert.Equal(10, result);
+        Assert.Equal(
+        [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        ], buffer.ToArray());
+    }
+    
+    [Fact]
+    public async Task ReadsTheDataFromMiddle()
+    {
+        using var ms = new MemoryStream([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        var storage = new StreamStorage2(ms);
+        storage.Seek(5, SeekOrigin.Begin);
+        
+        var buffer = new Memory<byte>(new byte[10]);
+        var result = await storage.ReadAsync(buffer);
+        
+        Assert.Equal(5, result);
+        Assert.Equal(
+        [
+            5, 6, 7, 8, 9, 0, 0, 0, 0, 0
+        ], buffer.ToArray());
+    }
+    
+    [Fact]
+    public async Task ReadsTheDataFromEnd()
+    {
+        using var ms = new MemoryStream([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        var storage = new StreamStorage2(ms);
+        storage.Seek(9, SeekOrigin.Begin);
+        
+        var buffer = new Memory<byte>(new byte[10]);
+        var result = await storage.ReadAsync(buffer);
+        
+        Assert.Equal(1, result);
+        Assert.Equal(
+        [
+            9, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ], buffer.ToArray());
+    }
+
+    [Fact]
+    public void LengthMatchesStreamPosition()
+    {
+        using var ms = new MemoryStream([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        var storage = new StreamStorage2(ms);
+        ms.Seek(1, SeekOrigin.Begin);
+        Assert.Equal(1, storage.Position);
+        
+        ms.Seek(3, SeekOrigin.Current);
+        Assert.Equal(4, storage.Position);
+        
+        ms.Seek(0, SeekOrigin.End);
+        Assert.Equal(10, storage.Position);
+    }
+}
