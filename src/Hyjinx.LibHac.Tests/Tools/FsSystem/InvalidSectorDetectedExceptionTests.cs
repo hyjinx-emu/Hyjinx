c7b91af96db1c2d60165b4f68b6948bc10a0581f
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-06-22 16:59:06 -0400

    Adding exception when invalid sector detected.

---
+++ src/Hyjinx.LibHac.Tests/Tools/FsSystem/InvalidSectorDetectedExceptionTests.cs
@@ -0,0 +1,19 @@
+﻿using LibHac.Tools.FsSystem;
+using Xunit;
+
+namespace LibHac.Tests.Tools.FsSystem;
+
+public class InvalidSectorDetectedExceptionTests
+{
+    [Fact]
+    public void ConstructorWorks()
+    {
+        var message = "Hello world";
+        var sectorIndex = 1;
+
+        var target = new InvalidSectorDetectedException(message, sectorIndex);
+        
+        Assert.Equal(message, target.Message);
+        Assert.Equal(sectorIndex, target.SectorIndex);
+    }
+}
---
+++ src/Hyjinx.LibHac/Tools/FsSystem/InvalidSectorDetectedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LibHac.Tools.FsSystem;
+
+/// <summary>
+/// Thrown when an invalid sector is detected.
+/// </summary>
+public class InvalidSectorDetectedException : Exception
+{
+    /// <summary>
+    /// The index of the sector which was invalid.
+    /// </summary>
+    public int SectorIndex { get; }
+    
+    /// <summary>
+    /// Initializes a new instance of the class.
+    /// </summary>
+    /// <param name="message">The message describing the error.</param>
+    /// <param name="sectorIndex">The index of the sector which was invalid.</param>
+    public InvalidSectorDetectedException(string message, int sectorIndex)
+        : base(message)
+    {
+        SectorIndex = sectorIndex;
+    }
+}
