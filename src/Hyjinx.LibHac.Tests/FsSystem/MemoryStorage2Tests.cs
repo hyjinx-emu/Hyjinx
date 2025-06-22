04a96824627c583d039ca05a0f1f5343d77f539c
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-06-21 21:56:37 -0400

    Adding memory storage support.

---
+++ src/Hyjinx.LibHac.Tests/FsSystem/MemoryStorage2Tests.cs
@@ -0,0 +1,54 @@
+﻿using LibHac.FsSystem;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibHac.Tests.FsSystem;
+
+public class MemoryStorage2Tests
+{
+    [Fact]
+    public async Task SeeksTheStorage()
+    {
+        byte[] data =
+        [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        ];
+
+        await using var target = new MemoryStorage2(data);
+        
+        target.Seek(1, SeekOrigin.Begin);
+        
+        var buffer = new Memory<byte>(new byte[32]);
+        var result = await target.ReadAsync(buffer);
+        
+        Assert.Equal(31, result);
+        Assert.Equal([
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
+            18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 0
+        ], buffer.ToArray());
+    }
+    
+    [Fact]
+    public async Task ReadsTheData()
+    {
+        byte[] data =
+        [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        ];
+
+        await using var target = new MemoryStorage2(data);
+
+        var buffer = new Memory<byte>(new byte[32]);
+        var result = await target.ReadAsync(buffer);
+        
+        Assert.Equal(32, result);
+        Assert.Equal([
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+        ], buffer.ToArray());
+    }
+}
---
+++ src/Hyjinx.LibHac/FsSystem/MemoryStorage2.cs
@@ -0,0 +1,45 @@
+﻿using LibHac.Tools.FsSystem;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibHac.FsSystem;
+
+/// <summary>
+/// An <see cref="IAsyncStorage"/> which sources data already held in memory.
+/// </summary>
+public class MemoryStorage2 : AsyncStorage
+{
+    private readonly MemoryStream _memoryStream;
+
+    public override long Length => _memoryStream.Length;
+    
+    public override long Position => _memoryStream.Position;
+
+    /// <summary>
+    /// Initializes a new instance of the class.
+    /// </summary>
+    /// <param name="data">The data for the storage.</param>
+    public MemoryStorage2(Span<byte> data)
+    {
+        _memoryStream = new MemoryStream(data.ToArray());
+    }
+    
+    public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        return await _memoryStream.ReadAsync(buffer, cancellationToken);
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return _memoryStream.Seek(offset, origin);
+    }
+
+    protected override async ValueTask DisposeAsyncCore()
+    {
+        await _memoryStream.DisposeAsync();
+        
+        await base.DisposeAsyncCore();
+    }
+}
