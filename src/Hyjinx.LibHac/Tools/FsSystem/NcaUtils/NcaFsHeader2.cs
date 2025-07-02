ea98e27431f4c310aa4ff95f0fb8238429bd5a84
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-07-02 08:17:16 -0400

    Updating comments.

--- src/Hyjinx.LibHac/Tools/FsSystem/NcaUtils/NcaFsHeader2.cs
+++ src/Hyjinx.LibHac/Tools/FsSystem/NcaUtils/NcaFsHeader2.cs
@@ -8,11 +8,36 @@ namespace LibHac.Tools.FsSystem.NcaUtils;
 [DebuggerDisplay("SectionStartOffset={SectionStartOffset}, SectionSize={SectionSize}")]
 public record NcaFsHeader2
 {
+    /// <summary>
+    /// The version.
+    /// </summary>
     public required short Version { get; init; }
+    
+    /// <summary>
+    /// The format type.
+    /// </summary>
+    /// <remarks>This is typically the type of file system used.</remarks>
     public required NcaFormatType FormatType { get; init; }
+    
+    /// <summary>
+    /// The type of hash to validate the data.
+    /// </summary>
     public required NcaHashType HashType { get; init; }
+    
+    /// <summary>
+    /// The zero-based offset upon which the section starts.
+    /// </summary>
     public required long SectionStartOffset { get; init; }
+    
+    /// <summary>
+    /// The length of the section.
+    /// </summary>
     public required long SectionSize { get; init; }
+    
+    /// <summary>
+    /// The data used for the checksum.
+    /// </summary>
+    /// <remarks>The contents of this array varies by the <see cref="HashType"/> used to validate the section.</remarks>
     public required byte[] Checksum { get; init; }
     public required byte[]? PatchInfo { get; init; }
     public required byte[]? SparseInfo { get; init; }
