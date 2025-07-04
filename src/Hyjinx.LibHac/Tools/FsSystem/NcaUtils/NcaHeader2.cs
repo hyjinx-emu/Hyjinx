c2301995472920bde70777a5bd1a65ff2a690ce8
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-07-04 13:21:58 -0400

    Adding comments.

--- src/Hyjinx.LibHac/Tools/FsSystem/NcaUtils/NcaHeader2.cs
+++ src/Hyjinx.LibHac/Tools/FsSystem/NcaUtils/NcaHeader2.cs
@@ -6,12 +6,43 @@
 /// <remarks>This header is for the archive itself, not the entries within the archive.</remarks>
 public record NcaHeader2
 {
+    /// <summary>
+    /// The magic value.
+    /// </summary>
     public required uint Magic { get; init; }
+    
+    /// <summary>
+    /// The distribution type.
+    /// </summary>
     public required DistributionType DistributionType { get; init; }
+    
+    /// <summary>
+    /// The content type.
+    /// </summary>
     public required NcaContentType ContentType { get; init; }
+    
+    /// <summary>
+    /// The size.
+    /// </summary>
     public required long Size { get; init; }
+    
+    /// <summary>
+    /// The title identifier.
+    /// </summary>
     public required ulong TitleId { get; init; }
+    
+    /// <summary>
+    /// The content index.
+    /// </summary>
     public required int ContentIndex { get; init; }
+    
+    /// <summary>
+    /// The version of the archive.
+    /// </summary>
     public required int Version { get; init; }
+    
+    /// <summary>
+    /// The format version.
+    /// </summary>
     public required NcaVersion FormatVersion { get; init; }
 }
