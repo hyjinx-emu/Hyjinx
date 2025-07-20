60c58d50be779e79258f1baefe2a6ebaa0204a07
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-07-20 16:39:42 -0400

    Stubbing Xci support.

---
+++ src/Hyjinx.LibHac/Fs/Sha256PartitionFileSystem2.cs
@@ -0,0 +1,21 @@
+﻿using LibHac.Fs.Fsa;
+using LibHac.Tools.Fs;
+using LibHac.Tools.FsSystem;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LibHac.Fs;
+
+public class Sha256PartitionFileSystem2 : FileSystem2
+{
+    public override Stream OpenFile(string fileName, FileAccess access = FileAccess.Read)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerable<FileInfoEx> EnumerateFileInfos(string? path = null, string? searchPattern = null,
+        SearchOptions options = SearchOptions.Default)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/Hyjinx.LibHac/Tools/Fs/Xci2.cs
@@ -0,0 +1,69 @@
+﻿using LibHac.Fs.Fsa;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibHac.Tools.Fs;
+
+/// <summary>
+/// Provides a mechanism to interact with executable cartridge image (XCI) files.
+/// </summary>
+public class Xci2
+{
+    /// <summary>
+    /// The underlying stream for the file.
+    /// </summary>
+    private Stream UnderlyingStream { get; }
+    
+    /// <summary>
+    /// The root file system.
+    /// </summary>
+    private IFileSystem2 RootFileSystem { get; }
+    
+    /// <summary>
+    /// Gets the header.
+    /// </summary>
+    public XciHeader Header { get; }
+
+
+    private Xci2(Stream stream, IFileSystem2 rootFileSystem, XciHeader header)
+    {
+        UnderlyingStream = stream;
+        RootFileSystem = rootFileSystem;
+        Header = header;
+    }
+    
+    /// <summary>
+    /// Loads the archive.
+    /// </summary>
+    /// <param name="stream">The stream to load.</param>
+    /// <param name="cancellationToken">The cancellation token to monitor for cancellation requests.</param>
+    /// <returns>The new <see cref="Xci2"/> file.</returns>
+    public static Task<Xci2> LoadAsync(Stream stream, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(new Xci2(stream, null!, new XciHeader(stream)));
+    }
+    
+    /// <summary>
+    /// Opens the file system.
+    /// </summary>
+    /// <param name="partition">The section.</param>
+    /// <param name="cancellationToken">The cancellation token to monitor for cancellation requests.</param>
+    /// <returns>The <see cref="IFileSystem"/> instance.</returns>
+    /// <exception cref="ArgumentException">The <paramref name="partition"/> does not exist.</exception>
+    public async Task<IFileSystem2> OpenFileSystemAsync(XciPartitionType partition, CancellationToken cancellationToken = default)
+    {
+        var stream = RootFileSystem.OpenFile($"/{partition.GetFileName()}");
+
+        try
+        {
+            return null!;
+        }
+        catch (Exception)
+        {
+            await stream.DisposeAsync();
+            throw;
+        }
+    }
+}
