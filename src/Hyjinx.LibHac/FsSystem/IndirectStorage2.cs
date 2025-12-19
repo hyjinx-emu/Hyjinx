9abb484d62fe7e61b2b6efeceec59a77b50f51e9
viper0179 <196049378+viper0179@users.noreply.github.com>
2025-12-18 20:35:56 -0500

    Adding more unit tests.

---
+++ src/Hyjinx.LibHac/FsSystem/IndirectStorage2.cs
@@ -0,0 +1,45 @@
+﻿using LibHac.Fs;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace LibHac.FsSystem;
+
+/// <summary>
+/// A storage which uses a relocation tree for mapping virtualized offsets to their physical offset on disk when reading data.
+/// </summary>
+public class IndirectStorage2 : Storage2
+{
+    /// <summary>
+    /// The definition for an <see cref="IndirectStorage2"/> bucket tree entry.
+    /// </summary>
+    /// <remarks>See: https://switchbrew.org/wiki/NCA#RomFs_Patching for more information.</remarks>
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct Entry : IBucketTreeEntry
+    {
+        public long VirtualOffset;
+        public long PhysicalOffset;
+        public int StorageIndex;
+
+        long IBucketTreeEntry.Offset => VirtualOffset;
+        
+        public override string ToString()
+        {
+            return $"(VirtualOffset={VirtualOffset}, PhysicalOffset={PhysicalOffset}, StorageIndex={StorageIndex})";
+        }
+    }
+
+    public override long Length => throw new NotImplementedException();
+    
+    public override long Position => throw new NotImplementedException();
+    
+    public override int Read(Span<byte> buffer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+}
