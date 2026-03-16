8feedcd29e0acb632400040a0f71b9b6ee125223
viper0179 <196049378+viper0179@users.noreply.github.com>
2026-03-16 17:40:25 -0400

    Updating returns.

--- src/Hyjinx.LibHac/Fs/Fsa/FileSystem2.Legacy.cs
+++ src/Hyjinx.LibHac/Fs/Fsa/FileSystem2.Legacy.cs
@@ -9,62 +9,63 @@ partial class FileSystem2 : FileSystem
 {
     protected override Result DoCreateFile(in Path path, long size, CreateFileOptions option)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoDeleteFile(in Path path)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoCreateDirectory(in Path path)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoDeleteDirectory(in Path path)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoDeleteDirectoryRecursively(in Path path)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoCleanDirectoryRecursively(in Path path)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoRenameFile(in Path currentPath, in Path newPath)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoRenameDirectory(in Path currentPath, in Path newPath)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoGetEntryType(out DirectoryEntryType entryType, in Path path)
     {
-        throw new NotSupportedException();
+        entryType = default;
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoOpenFile(ref UniqueRef<IFile> outFile, in Path path, OpenMode mode)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoOpenDirectory(ref UniqueRef<IDirectory> outDirectory, in Path path, OpenDirectoryMode mode)
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 
     protected override Result DoCommit()
     {
-        throw new NotSupportedException();
+        return ResultFs.NotImplemented.Log();
     }
 }
 
